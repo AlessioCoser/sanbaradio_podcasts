269fbb0b40cefc36693c6bee0b1285248a9c9246
alessiocoser <alessio.coser@xpeppers.com>
2016-12-09 00:05:01 +0100

    returns only valid Podcasts

--- test/unit/podcast-utils.js
+++ test/unit/podcast-utils.js
@@ -3,35 +3,45 @@ const podcastUtils = require('../../lib/podcast-utils')
 const Podcast = require('../../lib/podcast')
 
 describe('PodcastUtils', function () {
-  it('returns an empty array with null', function() {
-    var json = {}
-    var podcasts = podcastUtils.jsonToPods(json)
+  describe('jsonToPods', function () {
+    it('returns an empty array with null', function() {
+      var json = {}
+      var podcasts = podcastUtils.jsonToPods(json)
 
-    assert.deepEqual(podcasts, [])
-  })
+      assert.deepEqual(podcasts, [])
+    })
 
-  it('returns an empty array with no attributes', function() {
-    var json = {}
-    var podcasts = podcastUtils.jsonToPods(json)
+    it('returns an empty array with no attributes', function() {
+      var json = {}
+      var podcasts = podcastUtils.jsonToPods(json)
 
-    assert.deepEqual(podcasts, [])
-  })
+      assert.deepEqual(podcasts, [])
+    })
 
-  it('returns an empty array with no elements', function() {
-    var json = { items: []}
-    var podcasts = podcastUtils.jsonToPods(json)
+    it('returns an empty array with no elements', function() {
+      var json = { items: []}
+      var podcasts = podcastUtils.jsonToPods(json)
 
-    assert.deepEqual(podcasts, [])
-  })
+      assert.deepEqual(podcasts, [])
+    })
 
-  it('returns an array of Podcasts from a json', function () {
-    var json = {items: [ jsonItem('first'), jsonItem('second')]}
+    it('returns an array of Podcasts from a json', function () {
+      var json = {items: [ jsonItem('first'), jsonItem('second')]}
 
-    var podcasts = podcastUtils.jsonToPods(json)
-    var firstPodcast = podcasts[0]
+      var podcasts = podcastUtils.jsonToPods(json)
 
-    assert.equal(firstPodcast.title(), "first Title")
-    assert.equal(firstPodcast.file().url, "http://first.enc")
+      assert.equal(podcasts.length, 2)
+      assert.equal(podcasts[0].title(), "first Title")
+      assert.equal(podcasts[0].file().url, "http://first.enc")
+    })
+
+    it('returns only valid Podcasts', function () {
+      var json = {items: [ jsonItem('first'), invalidItem()]}
+
+      var podcasts = podcastUtils.jsonToPods(json)
+
+      assert.equal(podcasts.length, 1)
+    })
   })
 })
 
@@ -43,5 +53,12 @@ var jsonItem = function(name) {
     url: `http://${name}.url`,
     created: 1464018627000,
     enclosures: [{ url: `http://${name}.enc`, type: "audio/mpeg", length: 5000 }]
+  }
+}
+
+var invalidItem = function() {
+  return {
+    name: 'my name',
+    url: 'http://a.url'
   }
 }
